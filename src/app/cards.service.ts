51f1f97a37566b7ec23cc5a385e5125b8b1d0be8
burbuha <burbovskaja@gmail.com>
2021-02-05 17:59:05 +0300

    refactor: transfer conditions from service

--- src/app/cards.service.ts
+++ src/app/cards.service.ts
@@ -1,6 +1,6 @@
-import { randomFn, makePredicateFor } from './service.helpers';
+import { ALIVE, DEAD, LIFE, cells } from "./conditions";
+import { randomFn, makePredicateFor } from "./service.helpers";
 import { Injectable } from "@angular/core";
-
 
 export interface Cards {
   id: number;
@@ -11,58 +11,31 @@ export interface Cards {
 
 @Injectable()
 export class CardsService {
-  private cells = [
-    {
-      id: 1,
-      icon: "../assets/alive.jpg",
-      title: "Живая",
-      description: "и шевелится!",
-    },
-    {
-      id: 2,
-      icon: "../assets/dead.jpg",
-      title: "Мёртвая",
-      description: "или прикидывается",
-    },
-    {
-      id: 3,
-      icon: "../assets/life.jpg",
-      title: "Жизнь",
-      description: "Ку-ку!",
-    },
-  ]; 
-    
-  private ALIVE = this.cells[0];
-  private DEAD = this.cells[1];
-  private LIFE = this.cells[2];
-  
   private CELL_DEAD_COUNT: number = 3;
   private CELL_ALIVE_COUNT: number = 2;
 
-  
   private hasAlive = (arr) =>
-    makePredicateFor(arr, this.ALIVE, this.CELL_ALIVE_COUNT);
+    makePredicateFor(arr, ALIVE, this.CELL_ALIVE_COUNT);
 
-  private hasDead = (arr) =>
-    makePredicateFor(arr, this.DEAD, this.CELL_DEAD_COUNT);
+  private hasDead = (arr) => makePredicateFor(arr, DEAD, this.CELL_DEAD_COUNT);
 
   public cards: Cards[] = [];
 
   constructor() {}
 
   updateCard(randomCell = randomFn()) {
-    const newCell = this.cells[randomCell];
+    const newCell = cells[randomCell];
     const result = [...this.cards, newCell];
 
     if (this.hasDead(result)) {
-      const lastLiveIndx = result.map((item) => item.id).lastIndexOf(this.LIFE.id);
-      result.splice(lastLiveIndx, 1);
+      const lastLiveIndx = result.map((item) => item.id).lastIndexOf(LIFE.id);
+      lastLiveIndx > -1 && result.splice(lastLiveIndx, 1);
     }
 
     if (this.hasAlive(result)) {
-      result.push(this.LIFE);
+      result.push(LIFE);
     }
-    
+
     return result;
   }
 
---
+++ src/app/conditions.ts
@@ -0,0 +1,24 @@
+export const cells = [
+  {
+    id: 1,
+    icon: "../assets/alive.jpg",
+    title: "Живая",
+    description: "и шевелится!",
+  },
+  {
+    id: 2,
+    icon: "../assets/dead.jpg",
+    title: "Мёртвая",
+    description: "или прикидывается",
+  },
+  {
+    id: 3,
+    icon: "../assets/life.jpg",
+    title: "Жизнь",
+    description: "Ку-ку!",
+  },
+];
+
+export const ALIVE = cells[0];
+export const DEAD = cells[1];
+export const LIFE = cells[2];
