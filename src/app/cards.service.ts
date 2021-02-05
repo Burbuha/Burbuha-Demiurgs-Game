ca488771e875f0cf794e00e4bb470cce86bfa0ec
burbuha <burbovskaja@gmail.com>
2021-02-05 10:06:05 +0300

    fix: change the logic of the game

--- src/app/cards.service.ts
+++ src/app/cards.service.ts
@@ -1,32 +1,63 @@
 import { Injectable } from "@angular/core";
 
 export interface Cards {
-  id: number,
-  icon: string,
-  title: string,
-  description: string
+  id: number;
+  icon: string;
+  title: string;
+  description: string;
 }
 
-
-@Injectable({providedIn: 'root'})
-
+@Injectable({ providedIn: "root" })
 export class CardsService {
+  private cells = [
+    {
+      id: 1,
+      icon: "../assets/alive.jpg",
+      title: "Живая",
+      description: "и шевелится!",
+    },
+    {
+      id: 2,
+      icon: "../assets/dead.jpg",
+      title: "Мёртвая",
+      description: "или прикидывается",
+    },
+    {
+      id: 3,
+      icon: "../assets/life.jpg",
+      title: "Жизнь",
+      description: "Ку-ку!",
+    },
+  ];
 
-  private cells = [{id:1, icon: "../assets/alive.jpg", title: 'Живая', description: 'и шевелится!'},
-  {id:2, icon: "../assets/dead.jpg", title: 'Мёртвая', description: 'или прикидывается'},
-  {id:3, icon: "../assets/life.jpg", title: 'Жизнь', description: 'Ку-ку!'}];
+  constructor() {}
 
-  constructor() { }
-  
-  public cards: Cards[] = []
+  public cards: Cards[] = [];
 
   addCard(): void {
-    let i = Math.floor(Math.random()*2);
+    let i = Math.floor(Math.random() * 2);
     this.cards.push(this.cells[i]);
-    if (this.cards.length >= 3 && this.cards[this.cards.length-1].id === 2 && this.cards[this.cards.length-2].id === 2 && this.cards[this.cards.length-3].id === 2) {
-      this.cards.push(this.cells[1]);
-    } else if (this.cards.length >= 2 && this.cards[this.cards.length-1].id === 1 && this.cards[this.cards.length-2].id === 1){
+
+    let lengthArr = this.cards.length;
+
+    if (
+      lengthArr >= 3 &&
+      this.cards[lengthArr - 1].id === 2 &&
+      this.cards[lengthArr - 2].id === 2 &&
+      this.cards[lengthArr - 3].id === 2
+    ) {
+      for (let j = lengthArr - 1; j >= 0; j--) {
+        if (this.cards[j].id === 3) {
+          this.cards.splice(j, 1);
+          break;
+        }
+      }
+    } else if (
+      lengthArr >= 2 &&
+      this.cards[lengthArr - 1].id === 1 &&
+      this.cards[lengthArr - 2].id === 1
+    ) {
       this.cards.push(this.cells[2]);
-    }     
+    }
   }
-};
+}
